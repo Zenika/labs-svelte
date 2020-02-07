0ebd3ce29469e1976a9314534a299c6ea44ddcbb
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2020-02-07 12:11:24 +0100

    :sparkles: Step 4 : Ajouter des conditions d'affichages

--- become-svelte/src/Imc.svelte
+++ become-svelte/src/Imc.svelte
@@ -2,7 +2,14 @@
   export let poid = 80;
   export let taille = 1.80;
 
-
+  let imc = (poid / taille ** 2).toFixed(2)
 </script>
 
-<div>Votre IMC est de {(poid / taille ** 2).toFixed(2)}</div>
+<div>Votre IMC est de {imc}</div>
+{#if imc < 18}
+  <div>Vous êtes en sous poids</div>
+{:else if imc > 35}
+  <div>Vous êtes en sur poids</div>
+{:else}
+  <div>Quel corps svelte !</div>
+{/if}
