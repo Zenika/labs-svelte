0a7b647d2cdc9aca605cad4bdcc018e4cc9afb3f
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2020-02-12 13:58:09 +0100

    :tada: Template page pour calculer l'IMC

---
+++ template/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	let name = "World";
+</script>
+
+<main>
+	<h1>IMC Calculator</h1>
+	<p>Bonjour {name} ! Calculez votre IMC (Indice de Masse Corporelle)</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
