3e41397569f60c6c7338cb66144a7064d3d81352
Patrice de Saint Steban <patrice@desaintsteban.fr>
2022-01-14 23:41:22 +0100

    ✨ codelab pour sveltekit

---
+++ become-svelte/src/routes/about.svelte
@@ -0,0 +1,3 @@
+<p>
+Ce site a été créé par Patrice de Saint Steban et Anthony Le Goas lors de conférence SnowCamp
+</p>
---
+++ become-svelte/src/routes/recettes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    import recettes from "./recette.json";
+</script>
+
+<section class="recettes">
+    {#each recettes as item, index}
+        <article>
+            <h2><a href="/recettes/{index}">{item.titre}</a></h2>
+            <h3>⏱ {item.temps} 👨‍🍳 {item.difficulte} € {item.prix} 😋 {item.personnes} Personnes</h3>
+            <img src={item.image} alt={item.titre}>
+        </article>
+    {/each}
+</section>
