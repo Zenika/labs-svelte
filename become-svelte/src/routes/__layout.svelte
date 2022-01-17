b101e95aeacb883dc034c6953a7c9275325a63c7
Patrice de Saint Steban <patrice@desaintsteban.fr>
2022-01-17 11:07:43 +0100

    💄 amélioration du style

--- become-svelte/src/routes/__layout.svelte
+++ become-svelte/src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script>
 </script>
 <header>
+	<h1>Devenir Svelte avec Svelte</h1>
 	<nav>
 		<a href="/">Accueil</a>
 		<a href="/recettes">Recettes</a>
@@ -8,28 +9,5 @@
 	</nav>
 </header>
 <main>
-	<h1>Devenir Svelte avec Svelte</h1>
 	<slot></slot>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
