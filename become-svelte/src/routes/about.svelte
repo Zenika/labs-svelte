2149a47f9e38b0dfb18c76d850439b4ea9438f32
Patrice De Saint Steban <patrice.desaintsteban@zenika.com>
2022-01-30 21:44:59 +0000

    lors de la

--- become-svelte/src/routes/about.svelte
+++ become-svelte/src/routes/about.svelte
@@ -1,3 +1,3 @@
 <p>
-Ce site a été créé par Patrice de Saint Steban et Anthony Le Goas lors de conférence SnowCamp
+Ce site a été créé par Patrice de Saint Steban et Anthony Le Goas lors de la conférence SnowCamp
 </p>
