d893caf0bb00f55dbf03e36e6abd28b77edde2b9
Patrice De Saint Steban <patrice@desaintsteban.fr>
2022-04-19 15:31:56 +0200

    💥 Breaking change in sveltekit (#45)
    * 💥 Breaking change in sveltekit
    
    * Update become-svelte/package.json
    
    * ⬆️ Update netlify version
    
    * Update svelte version

--- become-svelte/src/routes/recettes/search.json.js
+++ become-svelte/src/routes/recettes/search.json.js
@@ -1,10 +1,11 @@
 import { searchRecipes, MarmitonQueryBuilder } from 'marmiton-api'
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function post(request) {
+export async function post({ request }) {
+    const body = await request.json()
     const qb = new MarmitonQueryBuilder();
     const query = qb
-    .withTitleContaining(request.body.query)
+    .withTitleContaining(body.query)
     .build()
     const recipes = await searchRecipes(query, { limit: 6 })
 
--- become-svelte/svelte.config.js
+++ become-svelte/svelte.config.js
@@ -6,9 +6,6 @@ const config = {
 		adapter: adapter({
 			split: false
 		}),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
 	}
 };
 
